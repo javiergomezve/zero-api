853788bf7749b560f11fd1b137c427b739449284
Javier Gomez <javiergomezve@gmail.com>
2021-05-27 22:37:32 -0500

    [ADD] Video: implement resource api

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Dtos\VideoPreview;
 use App\Http\Requests\VideoIndexRequest;
+use App\Http\Resources\VideoPreview as ResourcesVideoPreview;
 use App\Models\Video;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -12,11 +13,9 @@ class VideoController extends Controller
 {
     public function index(VideoIndexRequest $request)
     {
-        $videos = Video::last($request->getLimit(), $request->getPage())
-            ->get()
-            ->mapInto(VideoPreview::class);
+        $videos = Video::last($request->getLimit(), $request->getPage())->get();
 
-        return $videos;
+        return ResourcesVideoPreview::collection($videos);
     }
 
     public function show(Video $video)
